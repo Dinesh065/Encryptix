43392b03a7007ee2933c2899b9282527f9469599
Dinesh065 <dc9359349132@gmail.com>
2024-08-17 21:16:14 +0530

    Connected mongodb and created a user schema

---
+++ Task_3/Backend/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/dbmongo.js";
+dotenv.config({});
+
+const app = express();
+
+// app.get("/home",(req,res) => {
+//     return res.status(200).json({
+//         message: "I am coming from backend",
+//         success: true
+//     })
+// })
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true, 
+};
+app.use(cors(corsOptions));
+
+// Server setup
+const port = process.env.PORT || 5000; 
+app.listen(port, () => {
+    connectDB();
+  console.log(`Example app listening on port ${port}`);
+});
